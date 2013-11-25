ccea0ccce2aed9fa79039ad2aac4cbeb5542fba0
Evan Anderson <evan1124@gmail.com>
2013-11-25 18:31:07 -0500

    Update APNs and actually copy into build
    Change-Id: I0d29b5f9695d8891fc300efa5618d4d511bfa23f
    Signed-off-by: Evan Anderson <evan1124@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -1,3 +1,7 @@
 # Security Enhanced Linux
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
+
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/zero/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
